524afb9444d3fed7b1cc71b3bc81fd363680f7c4
Xiao Tan <tanxiao@unc.edu>
2024-07-28 17:10:17 +0800

    Track large files with Git LFS and other updates
    Former-commit-id: e7c98c65a552320155b36d2607f8d4347927f2c3

--- src/SiriusLucibus.js
+++ src/SiriusLucibus.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Cover from './assets/cover_siriuslucibus.png';
 
 const SiriusLucibus = () => {
-    return <h1>About Sirius Lucibus</h1>;
+    return (
+        <div>
+            <img className='cover' src={Cover} alt='Cover' />
+        </div>
+    )
 };
 
 export default SiriusLucibus;
